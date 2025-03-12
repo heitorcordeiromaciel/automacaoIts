bbbb15a8bf0a12db24e7e9e74f4734c17c57f6a5
Heitor Maciel <heitorcordeiro.maciel@gmail.com>
2025-03-12 11:14:53 -0300

    Release backupPfsenses

---
+++ scripts/pfsenses.js
@@ -0,0 +1,48 @@
+const { webkit } = require('playwright');
+
+(async () => {
+    console.log("Iniciando aplicação...");
+    const dataOntem = new Date();
+    dataOntem.setDate(dataOntem.getDate());
+    const dataOntemStr = dataOntem.toISOString().split('T')[0];
+    console.log("Acessando os pFsenses");
+    const browser = await webkit.launch();
+    const context = await browser.newContext({ ignoreHTTPSErrors: true });
+    const page = await context.newPage()
+    const backupPfsense = async (ip) => {
+        console.log(`Acessando: ${ip}`);
+        await page.goto(ip);
+        console.log('Tentando realizar login...');
+        await page.getByPlaceholder('Username').fill('julio');
+        await page.getByPlaceholder('Password').fill('imperatriz@135');
+        await page.locator('input[name="login"]').click()
+            .then(console.log('Logado com sucesso!'));
+        await page.getByRole('button', { name: 'Diagnostics' }).click();
+        console.log('Realizando backup das configurações do pFsense');
+        await page.getByRole('link', { name: 'Backup & Restore' }).click();
+
+        const downloadPromise = page.waitForEvent('download');
+        await page.locator('button[value="Download configuration as XML"]').click();
+        const download = await downloadPromise;
+        console.log('Backup concluido');
+
+        await download.saveAs(`\\\\192.168.1.31\\backup-4t\\BACKUP-PFSENSE-2024\\2025\\${dataOntemStr}\\` + download.suggestedFilename());
+
+    }
+    await backupPfsense('https://192.168.1.1');
+    await backupPfsense('https://10.100.20.1');
+    await backupPfsense('https://10.70.20.1');
+    await backupPfsense('https://10.70.30.1');
+    await backupPfsense('https://10.70.40.1');
+    await backupPfsense('https://192.168.50.1');
+    await backupPfsense('https://10.70.10.1');
+    await backupPfsense('https://192.168.10.1');
+    await backupPfsense('https://10.70.13.1');
+    console.log('Todos os backups concluidos');
+    
+    console.log('Finalizando tarefas...');
+    await context.close();
+    await browser.close();
+    console.log('Programa finalizado.');
+    process.exit(0);
+})();
