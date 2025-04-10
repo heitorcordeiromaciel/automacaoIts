0f5c631f7c3be157bb3431fc4605fcb93ef1c171
Heitor Maciel <heitorcordeiro.maciel@gmail.com>
2025-04-10 17:23:10 -0300

    release UI for ease of use

---
+++ apis/public/src/modules/criadorDeRamal.js
@@ -0,0 +1,105 @@
+const { chromium } = require('playwright');
+const credentials = require('../credentials.json');
+const logger = require('./../helpers/logger');
+
+const criaRamal = async (issabel, ramalStart, ramalEnd) => {
+
+	const { username, password } = credentials[issabel] || {};
+
+	if (!username || !password) {
+		return { success: false, message: 'Credenciais não encontradas para este Issabel.' };
+	}
+
+	logger.log("Iniciando aplicação...");
+	const browser = await chromium.launch();
+	const context = await browser.newContext({ ignoreHTTPSErrors: true });
+	const page = await context.newPage()
+
+	try {
+		logger.log("Tentando acessar o Issabel...");
+		logger.log(`Acessando: https://${issabel}`);
+		await page.goto(`https://${issabel}`).then(logger.log('Acesso concedido.'));
+		logger.log('Tentando realizar login...');
+		await page.getByPlaceholder('Usuário').fill(username);
+		await page.getByPlaceholder('Senha').fill(password);
+		await page.getByRole('button', { name: /Enviar/i }).click();
+		logger.log('Logado com sucesso!');
+		if ((issabel !== '192.168.1.60') && (issabel !== '10.70.40.250')) {
+			try {
+				await page.locator('.neo-modal-issabel-popup-close').click();
+				logger.log('popup fechado');
+			} catch (e) {
+				logger.log('nenhum popup');
+			}
+		};
+		logger.log('Acessando configurações do PBX');
+		await page.locator('[href="index.php?menu=pbxconfig"]').click();
+		await page.waitForTimeout(1000);
+		await page.getByRole('link', { name: 'Configurações do PBX' }).click();
+		await page.getByRole('link', { name: 'Ramais' }).click();
+		logger.log('Começando a criação de ramais.');
+		await page.waitForTimeout(2000);
+
+		logger.log('Criando ramais...');
+		for (let ramal = ramalStart; ramal <= ramalEnd; ramal++) {
+			logger.log(`Verificando se o ramal ${ramal} já existe...`);
+
+			const ramalExists = await page.isVisible(`[href="config.php?type=setup&display=extensions&extdisplay=${ramal}"]`);
+
+			if (ramalExists === true) {
+				logger.log(`Ramal ${ramal} já existe. Pulando...`);
+				continue;
+			} else if (ramalExists === false) {
+				logger.log(`Criando ramal ${ramal}...`);
+
+				await page.getByRole('link', { name: 'Adicionar Ramal' }).click();
+				await page.waitForTimeout(1000);
+
+				await page.getByRole('button', { name: 'Aplicar' }).click();
+				await page.waitForTimeout(2000);
+
+				await page.locator('#extension').fill(String(ramal));
+
+				await page.locator('#name').fill(String(ramal));
+
+				const clrPass = await page.locator('#devinfo_secret');
+				clrPass.fill('');
+				clrPass.fill('123456ab');
+
+				await page.locator('#devinfo_nat').selectOption({ value: 'yes' });
+				await page.getByRole('button', { name: 'Sempre' }).first().click();
+				await page.getByRole('button', { name: 'Sempre' }).nth(1).click();
+				await page.getByRole('button', { name: 'Sempre' }).nth(2).click();
+				await page.getByRole('button', { name: 'Sempre' }).nth(3).click();
+
+				await page.getByRole('button', { name: 'Aplicar' }).click();
+
+				await page.waitForTimeout(2000);
+
+				logger.log('Adicionando AccountCode');
+				await page.getByRole('link', { name: `${ramal}` }).click();
+
+				await page.waitForTimeout(2000);
+				await page.locator('#devinfo_accountcode').fill('MANUAL');
+
+				logger.log(`Salvando ramal ${ramal}...`);
+				await page.getByRole('button', { name: 'Aplicar' }).click();
+				await page.waitForTimeout(1000);
+				await page.locator('#button_reload').click();
+				logger.log(`Ramal ${ramal} criado com sucesso.`);
+			};
+		};
+
+		logger.log('Finalizando tarefas...');
+		await context.close();
+		await browser.close();
+		logger.log('Programa finalizado.');
+		return { success: true, message: 'Ramais criados com sucesso' }
+	} catch (e) {
+		await context.close();
+		await browser.close();
+		return { success: false, message: 'Ocorreu um erro durante a criação de ramal', e }
+	}
+}
+
+module.exports.criaRamal = criaRamal;
---
+++ apis/public/src/modules/vpnBlumenau.js
@@ -0,0 +1,53 @@
+const { chromium } = require('playwright');
+const logger = require('./../helpers/logger');
+
+async function restartVpn() {
+    try {
+        logger.log("Iniciando aplicação...");
+        const browser = await chromium.launch();
+        const context = await browser.newContext({ ignoreHTTPSErrors: true });
+        const page = await context.newPage();
+
+        logger.log("Acessando os pFsenses...");
+        await page.goto('https://192.168.1.1');
+
+        await page.getByPlaceholder('Username').fill('julio');
+        await page.getByPlaceholder('Password').fill('imperatriz@135');
+        await page.locator('input[name="login"]').click();
+        logger.log('Logado com sucesso!');
+
+        await page.getByRole('button', { name: 'Status' }).click();
+        logger.log('Reiniciando IPsec Blumenau...');
+        await page.locator('[href="/status_ipsec.php"]').click();
+        await page.waitForTimeout(500);
+
+        page.on('dialog', dialog => dialog.accept());
+
+        const rowLocators = page.locator(`tbody#ipsec-body tr:has(td:nth-child(2):text("BLUMENAU"))`);
+
+        const rowCount = await rowLocators.count();
+
+        if (rowCount === 0) {
+            logger.log('Nenhuma conexão VPN encontrada para BLUMENAU.');
+        } else {
+            logger.log(`Encontradas ${rowCount} conexões VPN para BLUMENAU. Reiniciando...`);
+            for (let i = 0; i < rowCount; i++) {
+                await rowLocators.nth(i).locator('a[title="Disconnect P1"]').click();
+                logger.log(`Desconectado P1 da instância ${i + 1}`);
+                await page.waitForTimeout(500);
+            }
+        }
+
+        logger.log('Finalizando tarefas...');
+        await context.close();
+        await browser.close();
+        logger.log('Programa finalizado.');
+
+        return { success: true, message: "Script executado com sucesso" };
+    } catch (error) {
+        console.error("Erro na execução do script:", error);
+        return { success: false, message: "Erro ao executar o script" };
+    }
+}
+
+module.exports.restartVpn = restartVpn
